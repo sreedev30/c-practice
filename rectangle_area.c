6c75173c119381af5e2fe2cb35d45ed9d14e7466
sreedev30 <kh.en.u4bca25154@kh.students.amrita.edu>
2025-09-25 03:58:42 -0400

    area of rectangle

---
+++ rectangle_area.c
@@ -0,0 +1,11 @@
+#include<stdio.h>
+void main()
+{
+float length,breadth,area;
+printf("enter length : ");
+scanf("%f",&length);
+printf("enter breadth:");
+scanf("%f",&breadth);
+area=length *breadth;
+printf("the area is : %.2f ",area);
+}
