ec71a119d1b885be6095ed01713483cfb96080a5
sreedev30 <kh.en.u4bca25154@kh.students.amrita.edu>
2025-09-25 05:40:48 -0400

    find circumferance of a circle

---
+++ circumferance_circl.c
@@ -0,0 +1,10 @@
+#include<stdio.h>
+void main()
+#define pi 3.14159
+{
+double radius,circumferance;
+printf("enter the radius of circle:");
+scanf("%lf",&radius);
+circumferance=2*pi * radius;
+printf("circumferancce of circle : %.2f ",circumferance);
+}
