453545ef856243317d5bfe3e720b72919d763c2b
sreedev30 <kh.en.u4bca25154@kh.students.amrita.edu>
2025-09-25 05:21:25 -0400

    larger of 2 numbers

---
+++ larger.c
@@ -0,0 +1,20 @@
+#include<stdio.h>
+void main()
+{
+double num1,num2;
+printf("enter first number:");
+scanf("%lf",&num1);
+printf("enter second number:");
+scanf("%lf",&num2);
+if (num1>num2)
+{
+printf("the largest number is : %lf ",num1);
+}
+else if (num2>num1)
+{
+printf("the largest number is : %lf ",num2);
+}
+else
+{
+printf("both numbers are equal");
+}}
