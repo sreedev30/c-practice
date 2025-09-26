4535fa49dd4b901afdac9b1392786ac702f14077
sreedev30 <kh.en.u4bca25154@kh.students.amrita.edu>
2025-09-26 07:20:08 -0400

    to calculate electricty bill using slabs

---
+++ bill.c
@@ -0,0 +1,20 @@
+#include<stdio.h>
+void main()
+{
+int units;
+float bill =0;
+printf("enter units consumed:");
+scanf("%d",&units);
+if(units<=100)
+{
+bill=units*5;
+}
+else if (units <=200)
+{
+bill = (100*5)+(units-100)*7;}
+else
+{
+bill=(100*5)+(100*7)+(units-200)*10;
+}
+printf("electricity bill = $%.2f",bill);
+}
