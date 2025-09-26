4ed271540647a4b1a442aefe0f7332d1ef8a3ee1
sreedev30 <kh.en.u4bca25154@kh.students.amrita.edu>
2025-09-26 04:38:45 -0400

    determine grade using marks

---
+++ grade.c
@@ -0,0 +1,27 @@
+#include<stdio.h>
+void main()
+{
+int marks;
+printf("enter marks:");
+scanf("%d",&marks);
+if (marks >=90)
+{
+printf("grade :A");
+}
+else if(marks >=80)
+{
+printf("grade : B");
+}
+else if(marks>=70)
+{
+printf("grade:C");
+}
+else if(marks>=60)
+{
+printf("grade :D");
+}
+else
+{
+printf("grade:F");
+}
+}
