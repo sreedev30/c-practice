1a4d8abd1cf76a5acbd6d673f6e5bc424db0943c
sreedev30 <kh.en.u4bca25154@kh.students.amrita.edu>
2025-09-26 03:24:22 -0400

    to check if a number is divisible by 5 and 11

---
+++ divisiblity.c
@@ -0,0 +1,19 @@
+#include<stdio.h>
+void main()
+{
+int num;
+printf("enter a number:");
+scanf("%d",&num;
+if(num%5==0)
+{
+printf("yes the number is div by 5",num);
+}
+else if(num%11==0)
+{
+printf("yes the number is div by 11",num);
+}
+else
+{
+printf("the number is not div by 5 or 11",num);
+}
+}
